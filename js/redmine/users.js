8db84ed09fbab2166255a2d1f5e2ce31719bb984
Konstantin Zolotarev <konstantin.zolotarev@gmail.com>
2013-08-22 18:51:33 +0600

    updated Users class

--- js/redmine/users.js
+++ js/redmine/users.js
@@ -6,127 +6,132 @@
  * @returns {Users}
  */
 com.rdHelper.Users = {
+
+    /**
+     * Flag showing that users are already loaded
+     *
+     * @var {boolean}
+     */
     loaded: false,
-    users: {}
-};
 
-/**
- * Clears list of users
- */
-com.rdHelper.Users.clear = function() {
-    this.users = {};
-    this.loaded = false;
-};
+    /**
+     * List of loaded users
+     *
+     * @var {Object}
+     */
+    users: {},
 
-/**
- * Store users data
- *
- * @param {?function()} callback
- */
-com.rdHelper.Users.store = function(callback) {
-    callback = callback || function() {};
-    chrome.storage.local.set({'users': this.users}, callback);
-};
+    /**
+     * Clear list of users and set {@link com.rdHelper.Users#loaded} to false
+     */
+    clear: function() {
+        this.users = {};
+        this.loaded = false;
+    },
 
-/**
- * Load users from storage
- *
- * @param {boolean=} reload
- * @param {?function(Object)} callback 
- */
-com.rdHelper.Users.load = function(reload, callback) {
-    if (arguments.length < 2 && typeof reload == "function") {
-        callback = reload;
-        reload = false;
-    }
-    callback = callback || function() {};
-    if (!reload && this.loaded) {
-        callback(this.users);
-    }
-    (function(obj) {
+    /**
+     * Store users in chrome local storage
+     *
+     * @param {function()=} [callback]
+     */
+    store: function(callback) {
+        callback = callback || function() {};
+        chrome.storage.local.set({'users': this.users}, callback);
+    },
+
+    /**
+     * Load users from storage
+     *
+     * @param {boolean=} [reload]
+     * @param {function(?Object)=} [callback]
+     */
+    load: function(reload, callback) {
+        if (arguments.length < 2 && typeof reload == "function") {
+            callback = reload;
+            reload = false;
+        }
+        callback = callback || function() {};
+        if (!reload && this.loaded) {
+            callback(this.users);
+            return;
+        }
         chrome.storage.local.get('users', function(item) {
             if (!item.users) {
-                obj.users = {};
-                callback();
+                this.users = {};
+                callback(null);
                 return;
             }
-            obj.users = item.users;
-            obj.loaded = true;
-            callback(obj.users);
-        });
-    })(this);
-};
+            this.users = item.users;
+            this.loaded = true;
+            callback(this.users);
+        }.bind(this));
+    },
 
-/**
- * Get all projects
- *
- * @param {boolean} reload
- * @param {function()=} callback
- */
-com.rdHelper.Users.all = function(reload, callback) {
-    if (arguments.length < 2 && typeof reload == "function") {
-        callback = reload;
-        reload = false;
-    }
-    if (this.loaded && !reload) {
-        callback(this.users);
-        return;
-    }
-    (function(obj) {
-        obj.load(function() {
-            callback(obj.users);
-        });
-        return;
-    })(this);
-};
+    /**
+     * Get all users. This is just reference to {@link com.rdHelper.Users#load} method
+     *
+     * @deprecated
+     * @param {boolean|function} [reload]
+     * @param {function()=} [callback]
+     */
+    all: function(reload, callback) {
+        this.load(reload, callback);
+    },
 
-/**
- * Add new user to the list 
- * 
- * @param {Object} user
- * @param {function()=} callback 
- */
-com.rdHelper.Users.push = function(user, callback) {
-    if (!user || !user.id || !user.name) {
-        return;
-    }
-    callback = callback || function() {};
-    (function(obj) {
-        obj.all(function() {
-            if (!obj.users[user.id]) {
-                obj.users[user.id] = user;
-                obj.store();
+    /**
+     * Add new user to the list
+     *
+     * @param {Object} user
+     * @param {function()=} [callback]
+     */
+    push: function(user, callback) {
+        if (!user || typeof user !== 'object' || !user.id || !user.name) {
+            return;
+        }
+        callback = callback || function() {};
+        this.load(function() {
+            if (!this.users[user.id]) {
+                this.users[user.id] = user;
+                this.store();
             }
             callback();
+        }.bind(this));
+    },
+
+    /**
+     * Grab users from issue object
+     *
+     * @param {Object} issue
+     */
+    grabFromIssue: function(issue) {
+        if (!issue || typeof issue !== 'object') {
             return;
-        });
-        return;
-    })(this);
-};
+        }
+        if (issue.assigned_to && typeof issue.assigned_to === 'object') {
+            this.push(issue.assigned_to);
+        }
+        if (issue.author && typeof issue.author === 'object') {
+            this.push(issue.author);
+        }
+    },
 
-/**
- * Grab users from issue
- *
- * @param {Object} issue
- */
-com.rdHelper.Users.grabFromIssue = function(issue) {
-    this.push(issue.assigned_to);
-    this.push(issue.author);
-};
-
-/**
- * Get username by userId
- *
- * @throws {Error} Error if users not loaded
- * @param {int} userId
- * @returns {String} 
- */
-com.rdHelper.Users.getNameById = function(userId) {
-    if (!this.loaded) {
-        throw Error("Users are not loaded !");
+    /**
+     * Get username by userId
+     *
+     * @throws {Error} Error if users not loaded
+     * @param {int} userId
+     * @returns {String}
+     */
+    getNameById: function(userId) {
+        if (!userId || typeof userId !== 'number') {
+            return "";
+        }
+        if (!this.loaded) {
+            throw Error("Users are not loaded !");
+        }
+        if (!this.users[userId]) {
+            return "Unknown user";
+        }
+        return this.users[userId].name;
     }
-    if (!this.users[userId]) {
-        return userId;
-    }
-    return this.users[userId].name;
 };
